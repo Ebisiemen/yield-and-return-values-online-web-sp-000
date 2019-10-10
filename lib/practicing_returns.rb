892291e248b5420a7624251c3fe03418888b724a
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-10 15:05:39 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -35,22 +35,3 @@ hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 
 
 
-
-
-
-
-# require 'pry'
-
-# def hello(array)
-#   i = 0
-#   collection = []
-#   while i < array.length
-#     collection << yield(array[i])
-#     yield(array[i])
-#     i += 1
-#   end
-#   collection 
-# end
-
-
-# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
